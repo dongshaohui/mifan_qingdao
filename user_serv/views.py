57680c7187e05dcda02b0d369ec3fd745eee8daa
dongshaohui <405989455@qq.com>
2016-04-26 12:28:53 +0800

    修改提交订单bug

--- user_serv/views.py
+++ user_serv/views.py
@@ -1262,7 +1262,7 @@ def upload_order(request):
 	# 运费
 	print "distance= ",distance
 	freight_price = 0
-	if float(total_price) < global_set.freight_thres:
+	if float(total_dish_price) < global_set.freight_thres:
 		freight_price = float("%.1f" % (float(distance) / (float)(1.6)))
 	# 佣金
 	commission = float("%.1f" %(float(total_dish_price) * float(shop_obj.commission)))
