98172207fae2771b88d05079d9689ae568c0613d
dongshaohui <405989455@qq.com>
2016-04-28 13:46:02 +0800

    修改xadmin链接地址

--- mifan/urls.py
+++ mifan/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mifan.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^xadmin/', include(xadmin.site.urls)),
+    url(r'^jdy_admin/', include(xadmin.site.urls)),
     # url(r'^admin/', include(admin.site.urls)),
 
     # 商家端接口（Android）
