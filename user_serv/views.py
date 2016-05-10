a061f9914ad6cfd09c337142a8121dc42b5214f7
dongshaohui <405989455@qq.com>
2016-05-11 01:06:33 +0800

    修改bug

--- user_serv/views.py
+++ user_serv/views.py
@@ -1298,7 +1298,7 @@ def upload_order(request):
 	if float(total_dish_price) < global_set.freight_thres:
 		freight_price = float("%.1f" % math.ceil((float(distance) / (float)(1.6))))
 	if int(consume_type) == 1:
-		freight_price == 0
+		freight_price = 0
 
 	# 佣金
 	commission = float("%.1f" %(float(total_dish_price) * float(shop_obj.commission)))
